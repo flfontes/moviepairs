d23d4a8860c9379e8370713aefc8f552f2d13474
flfontes <fabiolevifontes@gmail.com>
2023-08-31 16:59:31 +0100

    issue with buttons fixed

--- app.py
+++ app.py
@@ -5,23 +5,9 @@ from ttkbootstrap.constants import *
 from ttkbootstrap.dialogs import Messagebox
 
 
-# movie_list = ttb.Frame(root, bootstyle = "primary")
-# movie_list.pack(padx = 20, pady = (20, 10), fill = "both", expand = True)
-
-# actor_title = ttb.Label(movie_list, bootstyle = "primary inverse")
-# actor_title.config(text = "Actor vs Actor")
-# actor_title.pack(pady = 20)
-
-# actor01_button = ttb.Button(root, text = "First Actor", bootstyle = "primary outline")
-# actor01_button.pack(padx = (20, 10), pady = (10, 20), side = "left", expand = True, fill = "both")
-
-# actor02_button = ttb.Button(root, text = "Second Actor", bootstyle = "primary outline")
-# actor02_button.pack(padx = (10, 20), pady = (10, 20), side = "right", expand = True, fill = "both")
-
-
 class App(ttb.Frame):
     """
-    WIP:
+    WIP: A class handling the layout and functionality of the MoviePairs app.
     """
 
     # Main frame
@@ -29,15 +15,13 @@ class App(ttb.Frame):
         super().__init__(parent)
         self.pack(expand=True, fill="both")
 
-        Layout(self)
-
         button_frame = ttb.Frame(self)
         button_frame.pack(padx=20, pady=(20, 10), fill="x")
 
         search_button01 = ttb.Button(
             master=button_frame,
             text="Search First Actor",
-            command=lambda: actor_search(),
+            command=self.actor_search,
             bootstyle="primary outline",
         )
         search_button01.pack(padx=(0, 10), expand=True, fill="x", side="left")
@@ -45,7 +29,7 @@ class App(ttb.Frame):
         search_button02 = ttb.Button(
             master=button_frame,
             text="Search Second Actor",
-            command=lambda: actor_search(),
+            command=self.actor_search,
             bootstyle="primary outline",
         )
         search_button02.pack(padx=(10, 0), expand=True, fill="x", side="left")
@@ -53,37 +37,9 @@ class App(ttb.Frame):
         movie_list = ttb.Frame(self, bootstyle="primary")
         movie_list.pack(padx=20, pady=(10, 20), fill="both", expand=True)
 
+    # Actor Search
     def actor_search(self):
         pass
-
-
-class Layout:
-    """
-    WIP
-    """
-
-    def __init__(self, parent):
-        button_frame = ttb.Frame(parent)
-        button_frame.pack(padx=20, pady=(20, 10), fill="x")
-
-        search_button01 = ttb.Button(
-            master=button_frame,
-            text="Search First Actor",
-            command=actor_search,
-            bootstyle="primary outline",
-        )
-        search_button01.pack(padx=(0, 10), expand=True, fill="x", side="left")
-
-        search_button02 = ttb.Button(
-            master=button_frame,
-            text="Search Second Actor",
-            command=actor_search,
-            bootstyle="primary outline",
-        )
-        search_button02.pack(padx=(10, 0), expand=True, fill="x", side="right")
-
-        movie_list_frame = ttb.Frame(parent, bootstyle="primary")
-        movie_list_frame.pack(padx=20, pady=(10, 20), fill="both", expand=True)
 
 
 if __name__ == "__main__":
